f55c7e0232ae94dfbac0e78fef6bc8c5c6171910
olujedai <olujedai@gmail.com>
2019-08-27 21:05:14 +0100

    testing cors work around

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as helmet from 'helmet';
 import * as csurf from 'csurf';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
-import * as expressRequest from 'express-request-id';
+import * as expressRequestId from 'express-request-id';
 import * as bodyParser from 'body-parser';
 
 process.env.TZ = 'UTC';
@@ -19,30 +19,20 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/', app, document);
-  app.use(expressRequest());
+  app.use(expressRequestId());
   app.use(bodyParser());
   app.use(helmet());
   app.use(cookieParser());
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    optionsSuccessStatus: 200,
-  });
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    if (req.method === 'OPTION') {
-      res.header('Access-Control-Allow-Methods', 'GET,OPTION,POST,PUT,DELETE,PATCH');
-      return res.status(200).json({});
-    }
-    // if (req.method === 'POST') {
-    //   next();
-    // }
-    next();
-  });
-  app.use(session({secret: Math.random().toString(36).substring(7)}));
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   preflightContinue: true,
+  //   optionsSuccessStatus: 200,
+  // });
+  app.enableCors();
+  // app.use(session({secret: Math.random().toString(36).substring(7)}));
   app.use(csurf({
+    cookie: true,
     ignoreMethods: ['GET', 'POST', 'PUT', 'HEAD', 'OPTIONS'],
   }));
   await app.listen(Number(process.env.PORT || 3000));
