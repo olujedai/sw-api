6416d678fb533bbce2a91e15545a504380d6ed9b
olujedai <olujedai@gmail.com>
2019-08-27 18:41:11 +0100

    Enabled cors protection

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 process.env.TZ = 'UTC';
 
@@ -13,6 +15,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+  app.use(helmet());
+  app.use(csurf());
+  app.enableCors();
   await app.listen(Number(process.env.PORT || 3000));
 }
 bootstrap();
