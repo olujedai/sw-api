5ccb4c1e84bf0446961e455b2aa69c39fe9d8162
olujedai <olujedai@gmail.com>
2019-08-28 16:31:33 +0100

    Modified middleware positioning

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AllExceptionsFilter } from './all-exception.filter';
 import * as helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
 
 process.env.TZ = 'UTC';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.use(cookieParser());
   app.enableCors();
   app.useGlobalFilters(new AllExceptionsFilter());
   const options = new DocumentBuilder()
