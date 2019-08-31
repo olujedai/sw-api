cf969361cb78ed8b9f467a5f829efc28f9d755b9
olujedai <olujedai@gmail.com>
2019-08-31 18:46:30 +0100

    Changed the endpoints for GETting and POSTing comments for a movie from /comments/:movieId to movies/:movieId/comments and added these endpoints to the src/movies/movies.controller.ts

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,15 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import { Request } from 'express';
+import { Controller, Get, Query, Param, Req, Body, Post, Header } from '@nestjs/common';
+import { ApiUseTags } from '@nestjs/swagger';
+import { CommentService } from '../comment/comment.service';
+import { CommentParamDto } from '../comment/comment.param.dto';
+import { CommentDto } from '../comment/comment.dto';
+import { Comment } from '../comment/comment.entity';
 import { MoviesService } from './movies.service';
 import { MovieDto } from './movies.dto';
-import { ApiUseTags } from '@nestjs/swagger';
+import { UtilsService } from '../utils/utils.service';
 
 @ApiUseTags('sw-api')
 @Controller('movies')
 export class MoviesController {
-    constructor(private readonly movieService: MoviesService) {}
+    constructor(
+      private readonly movieService: MoviesService,
+      private readonly commentService: CommentService,
+      private readonly utilsService: UtilsService,
+    ) {}
 
     @Get()
     async getMovies(): Promise<MovieDto[]> {
       return await this.movieService.getMovies();
+    }
+
+    @Get(':movieId/comments')
+    async findAllMovieComments(@Param() param: CommentParamDto, @Query() query) {
+        const movieId = param.movieId;
+        const { skip, size } = query;
+        const filter = {
+            movieId,
+        };
+        const response = await this.commentService.findAll(skip, size, filter);
+        const comments = response[0];
+        const count = response[1];
+        return {
+            comments,
+            count,
+        };
+    }
+
+    @Post(':movieId/comments')
+    @Header('Content-Type', 'application/json')
+    async saveComment(@Param() param: CommentParamDto, @Req() request: Request, @Body() body: CommentDto): Promise<Comment> {
+        const movieId = param.movieId;
+        const ipAddress = this.utilsService.getIpAddress(request);
+        const {comment, commenter} = body;
+        return await this.commentService.createComment(movieId, ipAddress, comment, commenter);
     }
 }
